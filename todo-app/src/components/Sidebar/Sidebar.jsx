02d8a2cbdb0159037270029550a1b1ef610ba4c0
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-10 21:31:46 +0530

    sidebar completed

--- todo-app/src/components/Sidebar/Sidebar.jsx
+++ todo-app/src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,60 @@
 import "./Sidebar.css"
+import { NavLink } from "react-router-dom"
+
+    const defaults = {
+      padding:"20px",
+      textDecoration: "none",
+      color:"gray",
+    };
+
+    const active = {
+        padding:"20px",
+        borderRadius: "10px",
+        textDecoration: "none",
+        color: "white",
+        backgroundColor: "black"
+    };
+
+    const links = [
+      {
+        to:"/profile",
+        title: "Profile"
+      },
+      {
+        to:"/",
+        title: "All Tasks"
+      },
+      {
+        to:"/personal",
+        title: "Personal"
+      },
+      {
+        to:"/official",
+        title: "Official"
+      },
+      {
+        to:"/others",
+        title: "Others"
+      }
+    ] 
 
 export const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebarLogo">
-        {/* <img src="https://static.c.realme.com/IN/thread/1164218646675849216.png" alt="" /> */}
+        <img src="https://static.c.realme.com/IN/thread/1164218646675849216.png" alt="" />
+        <h1>Taskdly</h1>
       </div>
+    
       <div className="sidebarList">
-        <ul>
-          <li>ok ok</li>
-        </ul>
+        {links.map((link)=>(
+          <NavLink key={link.to} to={link.to} style={({isActive})=>(isActive ? active : defaults)} >
+            {link.title}  
+          </NavLink>
+        ))}
+        <NavLink style={defaults}>
+          Login/Logout 
+        </NavLink>
       </div>
     </div>
   )
