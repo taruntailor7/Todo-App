af43c675fc606c6aa50626c8a4761db9943b4886
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-11 00:14:39 +0530

    home page completed

--- todo-app/src/components/AllRoutes/AllRoutes.jsx
+++ todo-app/src/components/AllRoutes/AllRoutes.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router"
+import { Home } from "../../pages/Home/Home"
 import "./AllRoutes.css"
 
 export const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/profile" element></Route>
+      <Route path="/alltasks" element></Route>
+      <Route path="/personal" element></Route>
+      <Route path="/official" element></Route>
+      <Route path="/others" element></Route>
+    </Routes>
   )
 }
--- todo-app/src/components/Sidebar/Sidebar.jsx
+++ todo-app/src/components/Sidebar/Sidebar.jsx
@@ -6,13 +6,20 @@ import { NavLink } from "react-router-dom"
       textDecoration: "none",
       color:"gray",
     };
-
     const active = {
         padding:"20px",
         borderRadius: "10px",
         textDecoration: "none",
-        color: "white",
+        color: "#ffb000",
         backgroundColor: "black"
+    };
+
+    const logout = {
+      padding:"20px",
+      textDecoration: "none",
+      color:"white",
+      borderRadius: "10px",
+      backgroundColor: "#5876d8"
     };
 
     const links = [
@@ -21,7 +28,11 @@ import { NavLink } from "react-router-dom"
         title: "Profile"
       },
       {
-        to:"/",
+        to:"/createtask",
+        title: "Create Task"
+      },
+      {
+        to:"/alltasks",
         title: "All Tasks"
       },
       {
@@ -41,10 +52,12 @@ import { NavLink } from "react-router-dom"
 export const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebarLogo">
-        <img src="https://static.c.realme.com/IN/thread/1164218646675849216.png" alt="" />
-        <h1>Taskdly</h1>
-      </div>
+      <NavLink to="/" className="sidebarLogo">
+        {/* <div > */}
+          <img src="https://static.c.realme.com/IN/thread/1164218646675849216.png" alt="" />
+          <h1>Taskdly</h1>
+        {/* </div> */}
+      </NavLink>
     
       <div className="sidebarList">
         {links.map((link)=>(
@@ -52,7 +65,7 @@ export const Sidebar = () => {
             {link.title}  
           </NavLink>
         ))}
-        <NavLink style={defaults}>
+        <NavLink style={logout}>
           Login/Logout 
         </NavLink>
       </div>
